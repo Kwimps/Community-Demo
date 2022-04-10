1b01eee2c209c0deabed435a3fc0a2018b54c5ba
Kwimps <1944219984@qq.com>
2022-04-10 21:39:17 +0800

    私信列表的查看及私信具体内容的查看

---
+++ src/main/java/com/kwimps/community/service/MessageService.java
@@ -0,0 +1,18 @@
+package com.kwimps.community.service;
+
+import com.kwimps.community.entity.Message;
+
+import java.util.List;
+
+public interface MessageService {
+
+    public List<Message> findConversations(int userId, int offset, int limit);
+
+    public int findConversationCount(int userId);
+
+    public List<Message> findLetters(String conversationId, int offset, int limit);
+
+    public int findLetterCount(String conversationId);
+
+    public int findLetterUnreadCount(int userId, String conversationId);
+}
