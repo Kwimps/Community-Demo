0d648943f5a518cfbd63155d57fff2d23b725ae6
Kwimps <1944219984@qq.com>
2022-04-13 21:58:22 +0800

    引入kafka

--- src/test/java/com/kwimps/community/CommunityApplicationTests.java
+++ src/test/java/com/kwimps/community/CommunityApplicationTests.java
@@ -1,28 +1,12 @@
 package com.kwimps.community;
 
-import com.kwimps.community.dao.DiscussPostMapper;
-import com.kwimps.community.dao.LoginTicketMapper;
-import com.kwimps.community.dao.UserMapper;
-import com.kwimps.community.entity.DiscussPost;
-import com.kwimps.community.entity.LoginTicket;
-import com.kwimps.community.entity.User;
 import com.kwimps.community.service.LikeService;
-import com.kwimps.community.service.impl.testService;
 import com.kwimps.community.util.MailClient;
-import com.kwimps.community.util.SensitiveFilter;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.test.context.ContextConfiguration;
 import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
-
-import javax.annotation.Resource;
-import java.util.Date;
-import java.util.List;
 
 
 @SpringBootTest
@@ -39,6 +23,7 @@ class CommunityApplicationTests {
 
     @Autowired
     private RedisTemplate redisTemplate;
+    
 
     @Test
     void contextLoads() {
@@ -46,10 +31,9 @@ class CommunityApplicationTests {
 
     @Test
     public void userTest(){
-        String t = (String) redisTemplate.opsForValue().get("user:113");
-        System.out.println(t);
     }
 
-
-
 }
+
+
+
